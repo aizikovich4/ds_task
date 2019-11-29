6a6a8e7a452201e1ce1be7858e32189e140ecc0c
Aizikovich Boris <b.aizikovich@tools4brokers.com>
2019-11-29 09:59:35 +0300

    Add checking count of input arguments

--- calculator_primes.py
+++ calculator_primes.py
@@ -9,7 +9,7 @@ def check_params(argv):
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print 'test.py -i <inputfile> -o <outputfile>'
+            print 'use next parameters: -i <inputfile> -o <outputfile>'
             sys.exit()
         elif opt in ("-i", "--ifile"):
             inputfile = arg
@@ -54,4 +54,7 @@ def main(argv):
     print(values)
 
 if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print 'use next parameters: -i <inputfile> -o <outputfile>'
+        exit(1)
     main(sys.argv[1:])
