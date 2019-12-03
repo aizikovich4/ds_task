c532f7f9dc3df02eda466f5ded6ed65b6a854076
Aizikovich Boris <aizikovich4@gmail.com>
2019-12-04 00:06:35 +0300

    Refactoring test code

--- Dividers/Dividers.py
+++ Dividers/Dividers.py
@@ -1,23 +1,46 @@
 import sys, getopt
 import prime_divides
-import time
-from threading import Thread
+import time, os
 
-class PrintProcess(Thread):
-    def __init__(self, count):
-        Thread.__init__(self)
-        self.count = count
-        self.current_percent =0
-    def set(self, current):
-        if current:
-            self.current_percent = self.count/100 * current
+sys.path.append(os.getcwd())
+
+class PrimeDivide:
+    def __init__(self, input, output):
+        self.inputFile = input
+        self.outputFile = output
+        self.numbers =[]
+
+        try:
+            array = []
+            with open(self.inputFile, 'r') as f:
+                for line in f:
+                    try:
+                        array.append(int(line))
+                    except ValueError:
+                        pass
+            array = list(dict.fromkeys(array))
+            self.numbers = array
+        except IOError:
+            print("Input file not accessible: "+self.inputFile)
+
     def run(self):
-        old_percent = self.current_percent
-        while 1 or self.current_percent != 100:
-            if old_percent != self.current_percent:
-                print(self.current_percent)
-                old_percent = self.current_percent
-                time.sleep(10)
+        if not self.numbers:
+            return
+
+        current_counter = 0
+        with open(self.outputFile, 'w') as out:
+            for number in self.numbers:
+                divs = prime_divides.get_divides(number)
+                if not divs:
+                    continue
+                current_counter += 1
+                current_percent = current_counter / len(self.numbers) * 100
+                print(str(round(current_percent, 3)) + "%")
+                out.write(str(number) + " = 1")
+                for d in divs:
+                    out.write("*" + str(d))
+                out.write("\n")
+        print("100%")
 
 def check_params(argv):
     try:
@@ -41,45 +64,15 @@ def check_params(argv):
     return inputfile, outputfile
 
 
-def read_values(inputFile):
-    try:
-        array = []
-        with open(inputFile, 'r') as f:
-            for line in f:
-                try:
-                    array.append(int(line))
-                except ValueError:
-                    pass
-        # array = list(dict.fromkeys(array))
-        return array
-    except IOError:
-        print("Input file not accessible: "+inputFile)
-        return
-
-
 def main(argv):
     inputFile, outputFile = check_params(argv)
-    print("Input file: "+ inputFile)
-    print("Output file: "+ outputFile)
+    print("Input file: " + inputFile)
+    print("Output file: " + outputFile)
 
-    values = read_values(inputFile)
-    if not values:
-        exit(1)
+    dividersTest = PrimeDivide(inputFile, outputFile)
+    dividersTest.run()
 
-    current_counter = 0
-    with open(outputFile, 'w') as out:
-        for number in values:
-            divs = prime_divides.get_divides(number)
-            if not divs:
-                continue
-            current_counter += 1
-            current_percent = current_counter/len(values) * 100
-            print(str(round(current_percent, 3))+"%")
-            out.write(str(number)+" = 1")
-            for d in divs:
-                out.write("*"+str(d))
-            out.write("\n")
-    print("100%")
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 5:
--- multiply_library/test.py
+++
@@ -1,4 +0,0 @@
-import multiply_library
-
-#multiply_library.get_divides([32231],"BORIS!!!.txt")
-print(multiply_library.write_divides_to_File([118,635,32424, 3654, 98756, 654,78,579,666,657,32231,18,65,3224, 3654, 98756, 654,78,579,666,657,32231,18,65,3224, 3654, 98756, 654,78,579,666,657,322312,98798712222],"BORIS!!!.txt"))
